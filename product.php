0a75147beeeccba073910b25d41b46c72aa896e7
rudra-blazee <sumitanand3098@gmail.com>
2022-03-29 23:24:31 +0530

    font change

--- product.php
+++ product.php
@@ -55,8 +55,78 @@ include "header.php";
 				<div class="row">
 					<!-- Product main img -->
 					
-					<div class="col-md-5">
+					<?php 
+								include 'db.php';
+								$product_id = $_GET['p'];
+								
+								$sql = " SELECT * FROM products ";
+								$sql = " SELECT * FROM products WHERE product_id = $product_id";
+								if (!$con) {
+									die("Connection failed: " . mysqli_connect_error());
+								}
+								$result = mysqli_query($con, $sql);
+								if (mysqli_num_rows($result) > 0) 
+								{
+									while($row = mysqli_fetch_assoc($result)) 
+									{
+									echo '
+									
+                                    
+                                
+                                <div class="col-md-5 col-md-push-2">
+                                <div id="product-main-img">
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'" alt="">
+                                    </div>
+
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'" alt="">
+                                    </div>
+
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'" alt="">
+                                    </div>
+
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                                
+                                <div class="col-md-2  col-md-pull-5">
+                                <div id="product-imgs">
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'" alt="">
+                                    </div>
+
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'" alt="">
+                                    </div>
+
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'g" alt="">
+                                    </div>
+
+                                    <div class="product-preview">
+                                        <img src="product_images/'.$row['product_image'].'" alt="">
+                                    </div>
+                                </div>
+                            </div>
+
+                                 
+									';
+                                    
+									?>
+									<!-- FlexSlider -->
+									
+									<?php 
+									echo '
+									
+                                    
+                                   
+                    <div class="col-md-5">
 						<div class="product-details">
+							<h2 class="product-name">'.$row['product_title'].'</h2>
 							<div>
 								<div class="product-rating">
 									<i class="fa fa-star"></i>
@@ -68,8 +138,10 @@ include "header.php";
 								<a class="review-link" href="#review-form">10 Review(s) | Add your review</a>
 							</div>
 							<div>
+								<h3 class="product-price">$'.$row['product_price'].'<del class="product-old-price">$990.00</del></h3>
 								<span class="product-available">In Stock</span>
 							</div>
+							<p>'.$row['product_desc'].'</p>
 
 							<div class="product-options">
 								<label>
@@ -370,6 +442,77 @@ include "header.php";
 							<h3 class="title">Related Products</h3>
 							
 						</div>
+					</div>
+                    ';
+									$_SESSION['product_id'] = $row['product_id'];
+									}
+								} 
+								?>	
+								<?php
+                    include 'db.php';
+								$product_id = $_GET['p'];
+                    
+					$product_query = "SELECT * FROM products,categories WHERE product_cat=cat_id AND product_id BETWEEN $product_id AND $product_id+3";
+                $run_query = mysqli_query($con,$product_query);
+                if(mysqli_num_rows($run_query) > 0){
+
+                    while($row = mysqli_fetch_array($run_query)){
+                        $pro_id    = $row['product_id'];
+                        $pro_cat   = $row['product_cat'];
+                        $pro_brand = $row['product_brand'];
+                        $pro_title = $row['product_title'];
+                        $pro_price = $row['product_price'];
+                        $pro_image = $row['product_image'];
+
+                        $cat_name = $row["cat_title"];
+
+                        echo "
+				
+                        
+                                <div class='col-md-3 col-xs-6'>
+								<a href='product.php?p=$pro_id'><div class='product'>
+									<div class='product-img'>
+										<img src='product_images/$pro_image' style='max-height: 170px;' alt=''>
+										<div class='product-label'>
+											<span class='sale'>-30%</span>
+											<span class='new'>NEW</span>
+										</div>
+									</div></a>
+									<div class='product-body'>
+										<p class='product-category'>$cat_name</p>
+										<h3 class='product-name header-cart-item-name'><a href='product.php?p=$pro_id'>$pro_title</a></h3>
+										<h4 class='product-price header-cart-item-info'>$pro_price<del class='product-old-price'>$990.00</del></h4>
+										<div class='product-rating'>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+										</div>
+										<div class='product-btns'>
+											<button class='add-to-wishlist'><i class='fa fa-heart-o'></i><span class='tooltipp'>add to wishlist</span></button>
+											<button class='add-to-compare'><i class='fa fa-exchange'></i><span class='tooltipp'>add to compare</span></button>
+											<button class='quick-view'><i class='fa fa-eye'></i><span class='tooltipp'>quick view</span></button>
+										</div>
+									</div>
+									<div class='add-to-cart'>
+										<button pid='$pro_id' id='product' class='add-to-cart-btn block2-btn-towishlist' href='#'><i class='fa fa-shopping-cart'></i> add to cart</button>
+									</div>
+								</div>
+                                </div>
+							
+                        
+			";
+		}
+        ;
+      
+}
+?>
+					<!-- product -->
+					
+					<!-- /product -->
+
+				</div>
 				<!-- /row -->
                 
 			</div>
@@ -382,3 +525,8 @@ include "header.php";
 		<!-- /NEWSLETTER -->
 
 		<!-- FOOTER -->
+<?php
+include "newslettter.php";
+include "footer.php";
+
+?>
